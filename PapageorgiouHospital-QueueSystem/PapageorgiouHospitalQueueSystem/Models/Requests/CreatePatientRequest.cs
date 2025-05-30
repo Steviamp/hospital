f16a45cc9922faa6d2b86c6219a076367b080287
Steviamp <74257777+Steviamp@users.noreply.github.com>
2025-05-30 10:25:39 +0300

    Delete PapageorgiouHospital-QueueSystem directory

--- PapageorgiouHospital-QueueSystem/PapageorgiouHospitalQueueSystem/Models/Requests/CreatePatientRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PapageorgiouHospitalQueueSystem.Models.Requests
-{
-    public class CreatePatientRequest
-    {
-        public string Name { get; set; } = string.Empty;
-        public string PatientNumber { get; set; } = string.Empty;
-        public string? Comment { get; set; }
-    }
-}
